79f2eeb04db4bbb9aff7a3bc6b121a1bfe3d7488
Valentin Suarez <164792863+Valen718@users.noreply.github.com>
2024-11-24 11:46:44 -0300

    Add files via upload

---
+++ OrdenarListas.py
@@ -0,0 +1,6 @@
+numeros = [76, 48, 92, 33, 55, 12, 81, 87, 60, 21]
+numeros.sort()
+print(*numeros, sep="-")
+
+#Aprendi a como mostrar listas sin corchetes usando print, el " * " lo que hace es eliminar corchetes
+#Y el " sep='-' " para especificar el separador entre los elementos de la lista.
